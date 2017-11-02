af220a9b06a37c92d6c2072acd3722ac3975630c
Kyle Reubendale <kylereubendale@Kyles-MacBook-Air.local>
2017-11-02 15:32:52 -0600

    nav and footer and login

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import "./App.css";
 import Upload from './components/Upload';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import Nav from './components/Nav'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
         <BrowserRouter>
           <Switch>
-            <Route path="/" exact component={Login} />
+            {/* <Route path="/" exact component={Login} /> */}
             <Route path="/upload" component={Upload} />
             <Route path="/dashboard" component={Dashboard} />
           </Switch>
         </BrowserRouter>
+        <Footer />
       </div>
     )
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './styles/Nav.css'
 
-class Nav extends React.Component {
-	render (
-		
+const Nav = () => {
+	return (
+
+		<div id="NavWrapper">
+		<div id="Nav1"></div>
+		<div id="Nav2">Grad Tracker</div>
+		<div id="Nav3"></div>
+		<div id="Nav4">About</div>
+		<div id="Nav5">Help</div>
+		<div id="fbookButton" className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false" scope="public_profile,email" data-onlogin="checkLoginState"></div>
+		</div>
+
 	)
 }
 
